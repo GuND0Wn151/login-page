08843d506f357d27f8623788a528539a401835d8
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-03-06 10:59:24 +0530

    include login Icon

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
 'use strict';
-//Buttons
+//Select Elements
 const containerLogin = document.querySelector('.container');
 const btnLogin = document.querySelector('.btn-login');
 const btnContainerClose = document.querySelector('.btn-close');
 const btnSubmit = document.querySelector('.btn-submit');
+const iconLogin = document.querySelector('.material-symbols-outlined');
 
 //Messages
 const labelMessage = document.querySelector('.lebal-message');
@@ -13,9 +14,9 @@ const inputName = document.querySelector('.input__name');
 const inputPassword = document.querySelector('.input__password');
 
 // Play Audio Function
-const playAudio = function(audioPath){
-new Audio(`${audioPath}`).play();
-}
+const playAudio = function (audioPath) {
+  new Audio(`${audioPath}`).play();
+};
 
 const openLoginPage = () => {
   containerLogin.classList.remove('hidden');
@@ -27,11 +28,13 @@ const closeLoginPage = () => {
 btnLogin.addEventListener('click', () => {
   openLoginPage();
   btnLogin.style.opacity = 0;
+  iconLogin.style.opacity = 0;
 });
 
 btnContainerClose.addEventListener('click', () => {
   closeLoginPage();
   btnLogin.style.opacity = 100;
+  iconLogin.style.opacity = 100;
 });
 
 document.addEventListener('keydown', e => {
@@ -46,16 +49,15 @@ btnSubmit.addEventListener('click', () => {
   const password = inputPassword.value;
 
   if (name && password) {
-    playAudio('sounds/login-successfully.mp3')
+    playAudio('sounds/login-successfully.mp3');
     btnLogin.style.fontSize = '2rem';
     btnLogin.textContent = `Welcome: ${name}`;
     btnLogin.style.opacity = 100;
-    btnLogin.style.color = 'Green';
     inputName.value = inputPassword.value = '';
     closeLoginPage();
     //////////////////////////////////
   } else {
-    playAudio('sounds/login-denied.mp3')
+    playAudio('sounds/login-denied.mp3');
     labelMessage.textContent = 'Invalid Name & Password';
     labelMessage.style.color = 'red';
   }
