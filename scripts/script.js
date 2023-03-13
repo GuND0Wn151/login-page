f26bb856cd4fa318a8f6d7218a402ba7596d9853
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-03-13 23:37:52 +0530

    add boostrap spinner in loading message

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,50 @@ const inputPassword = document.querySelector('.input__password');
 //body
 const body = document.querySelector('body');
 
+// Function login
+const functionLogin = function () {
+  const name = inputName.value;
+  const password = inputPassword.value;
+  inputName.value = inputPassword.value = '';
+  const randomNumber = Math.trunc(Math.random() * 10) + 1;
+
+  let timer = randomNumber;
+  body.style.cursor = 'wait';
+
+  const p = document.createElement('p');
+  p.innerHTML = `<button class="btn btn-primary" type="button" disabled>
+  <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+  Loading...
+</button>`;
+
+  containerLogin.prepend(p);
+
+  const loadingTimer = setInterval(() => {
+    timer--;
+
+    if (timer === 0) {
+      body.style.cursor = 'text';
+      clearInterval(loadingTimer);
+      if (name && password) {
+        //Local storage
+        localStorage.setItem('name', name);
+        localStorage.setItem('password', password);
+        localStorage.getItem(name);
+        localStorage.getItem(password);
+        playAudio('sounds/login-successfully.mp3');
+        btnLogin.style.fontSize = '2rem';
+        btnLogin.textContent = `Welcome: ${name}`;
+        btnLogin.style.opacity = 100;
+        closeLoginPage();
+      } else {
+        playAudio('sounds/login-denied.mp3');
+        labelMessage.textContent = 'Invalid Name & Password';
+        labelMessage.style.color = 'red';
+        p.remove();
+      }
+    }
+  }, 1000);
+};
 // Play Audio Function
 const playAudio = function (audioPath) {
   new Audio(`${audioPath}`).play();
@@ -46,39 +90,5 @@ document.addEventListener('keydown', e => {
     btnLogin.style.opacity = 100;
   }
 });
-btnSubmit.addEventListener('click', () => {
-  const name = inputName.value;
-  const password = inputPassword.value;
-  inputName.value = inputPassword.value = '';
-  const randomNumber = Math.trunc(Math.random() * 10) + 1;
-
-  let timer = randomNumber;
-  body.style.cursor = 'wait';
-
-  const p = document.createElement('p');
-  p.textContent = `Loading...`;
-  p.classList.add('label__loading');
-
-  containerLogin.prepend(p);
-
-  const loadingTimer = setInterval(() => {
-    timer--;
-    console.log(timer);
-
-    if (timer === 0) {
-      body.style.cursor = 'text';
-      clearInterval(loadingTimer);
-      if (name && password) {
-        playAudio('sounds/login-successfully.mp3');
-        btnLogin.style.fontSize = '2rem';
-        btnLogin.textContent = `Welcome: ${name}`;
-        btnLogin.style.opacity = 100;
-        closeLoginPage();
-      } else {
-        playAudio('sounds/login-denied.mp3');
-        labelMessage.textContent = 'Invalid Name & Password';
-        labelMessage.style.color = 'red';
-      }
-    }
-  }, 1000);
-});
+document.addEventListener('keypress', function () {});
+btnSubmit.addEventListener('click', functionLogin);
